85b65e614a9799059708a647405424a61302fa23
BbongLee <jebon0722@gmail.com>
2017-09-19 20:28:51 +0900

    #4 Hello Piccasso :)

--- app/src/main/java/com/bepro/globe22/firebasedbexample/MainActivity.java
+++ app/src/main/java/com/bepro/globe22/firebasedbexample/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
 public class MainActivity extends AppCompatActivity {
     private TextView mNameTextView;
     private TextView mGithubTextView;
@@ -18,5 +20,12 @@ public class MainActivity extends AppCompatActivity {
         mNameTextView = (TextView) findViewById(R.id.profile_name);
         mGithubTextView = (TextView) findViewById(R.id.profile_github);
         mProfilImageView = (ImageView) findViewById(R.id.profile_image);
+
+        ImageView mProfileImageView;
+        Picasso.with(this)
+                .load("http://i.imgur.com/DvpvklR.png")
+                .centerCrop() //가운데만 잘라서
+                .resize(100,100) //가로세로 100픽셀
+                .into(mProfilImageView);
     }
 }
